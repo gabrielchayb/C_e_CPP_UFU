bb844b709e51e305912285e4dccfcc1becc36cf9
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-22 19:33:07 -0300

    calculo de media com desvio padrao

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,13 +1,28 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
+int main()
+{
+    double var[10];
+    double m,dp;
 
-int main(){
-    float F, C, K;
-    cout << "Celsius: ";
-    cin >> C;
-    F = 9 * C / 5.0 + 32;
-    K = C + 273,15;
-    cout << "celsius: " << C << "f:" << F << "k: " << K << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << "Digite o valor " << i+1 << ": ";
+        cin >> var[i];
+        m += var[i];
+    }
 
+    m = m/10;
+
+    for (int i = 0; i < 10; ++i) {
+        dp+= pow(var[i] - m, 2);
+    }
+    dp= sqrt(dp / 10);
+
+    cout << "A média é " << m << endl;
+    cout << "O desvio padrão é " << dp<< endl;
+
+    return 0;
 }

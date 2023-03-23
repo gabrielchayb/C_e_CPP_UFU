1c833f80111a28bf045cf23c32d9f4cfc8fa1838
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-23 16:41:55 -0300

    matryoska - ordenação e comparação

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,23 +2,38 @@
 using namespace std;
 
 
-vector<long long int>v;
+vector<long long int>v, aux, novo;
 
 int main()
 {
-    int N;
-    long long int x; 
-    cin >> N;
-    while(N--){
+    
+    int N,x;
+    cin >> N; 
+    for (int i=0; i<N; i++){
         cin >> x; 
         v.push_back(x);
+        aux.push_back(x);
+        
+
+    } 
+
+    sort(aux.begin(),aux.end());
+    for (int i=0; i<N; i++){
+        if(v[i] != aux[i]){
+            novo.push_back(i);
+        }
+
     }
 
-    sort(v.begin(), v.end());
-    for (int i =0; i<v.size()-1; i++){
-        cout << v[i] << " ";
+    cout << novo.size() << endl; // 2
+    for(int i=0;i<novo.size()-1;i++){
+        cout << novo[i] << " ";// 1 
     }
-    cout << v[v.size()-1] << endl;
+    cout << novo[novo.size()-1] << endl; // 7 
+
+
+
+
 
     return 0;
 

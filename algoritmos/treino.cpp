73d5df2042590a061df0ae90a42d41faf38e39ba
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-23 01:04:41 -0300

    ordenação mas bugada

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,28 +1,15 @@
-#include <iostream>
-#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    double var[10];
-    double m,dp;
-
-    for (int i = 0; i < 10; ++i) {
-        cout << "Digite o valor " << i+1 << ": ";
-        cin >> var[i];
-        m += var[i];
+    double v[5];
+    
+    for(int i=0; i<5; i++){
+        cin >> v[i];
     }
 
-    m = m/10;
-
-    for (int i = 0; i < 10; ++i) {
-        dp+= pow(var[i] - m, 2);
-    }
-    dp= sqrt(dp / 10);
-
-    cout << "A média é " << m << endl;
-    cout << "O desvio padrão é " << dp<< endl;
-
+    cout << v << endl;
+    
     return 0;
 }

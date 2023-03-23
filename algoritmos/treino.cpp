39c18a5638ea5e38e9350e239db9fa67fc83f146
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-23 19:25:04 -0300

    jogo de papel e tesoura com um monte de elseif

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,45 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
 
+int main(){
 
-vector<long long int>v;
-
-int main()
-{
-    
-    int N,x;
-    cin >> N; 
-    for (int i=0; i<3; i++){
-        cin >> x; 
-        v.push_back(x);
-       
-        
-
-    } 
-
-    sort(v.begin(), v.end());
-
-    int qtd=0; 
-
-    for (int i=0; i<v.size(); i++){
-        N -= v[i]; 
-
-        if (N>=0){
-            qtd++;
-        }
-
+    int A,B,R,P,S; 
+    R = 1;
+    P = 2; 
+    S = 3; 
+    cout << "Jogador 1 (digite 1 para R, 2 para P e 3 para S): ";
+    cin >> A; 
+    cout << "Jogador 2 (digite 1 para R, 2 para P e 3 para S): "; 
+    cin >> B; 
+    if(A == 1 && B==2){
+        cout << "jogador 2 venceu" << endl;
     }
-
-    cout << qtd << endl;
-
-
-
-
-
-    return 0;
-
-
-
-
+    else if(A == 1 && B==3){
+        cout << "jogador 1 venceu" << endl;
+    }
+    else if( A == 1 && B==1){
+        cout << "jogo empatado" << endl;
+    }
+    else if(A == 2 && B == 1){
+        cout << "Jogador 1 ganhou" << endl;
+    }
+    else if(A == 2 && B == 2){
+        cout << "Jogo empatado" << endl;
+    }
+    else if(A == 2 && B == 3){
+        cout << "Jogador 2 venceu" << endl;
+    }
+    else if(A == 3 && B == 3){
+        cout << "Jogao empatado" << endl;
+    }
 
 }

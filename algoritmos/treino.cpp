69fa7bc9cf3620fe2f5e7ef2b18f86bc36d2acb1
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-23 16:32:16 -0300

    ordenação simples de um vetor int

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,15 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+vector<long long int>v;
+
 int main()
 {
-    double v[5];
-    
-    for(int i=0; i<5; i++){
-        cin >> v[i];
+    int N;
+    long long int x; 
+    cin >> N;
+    while(N--){
+        cin >> x; 
+        v.push_back(x);
     }
 
-    cout << v << endl;
-    
+    sort(v.begin(), v.end());
+    for (int i =0; i<v.size()-1; i++){
+        cout << v[i] << " ";
+    }
+    cout << v[v.size()-1] << endl;
+
     return 0;
+
+
+
+
+
 }

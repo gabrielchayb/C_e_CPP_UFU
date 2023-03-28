c537adfbc0ddf2756afa4a4d90aa143d19beb740
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-28 16:21:11 -0300

    exercicio - neps academy é sucesso com for

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 
 int main(){
-	int n;
-    for (int i = 0; i<6; i++){
-        if (i == 2){
-            continue; //continue = 0 1 3 4 5 
-                     // break = 0 1 
-        }
-    cout << i << endl;
-    }
+	int X;
+    cin >> X;
+    for (int i = 0; i<X; i++){
+        cout << "NepsAcademy eh Sucesso" << endl; 
+    };
+    
+
 }

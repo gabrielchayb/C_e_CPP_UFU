852214caad05b98e6529b3c595d218545eaa4645
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-28 14:55:49 -0300

    estudo : for simples c++

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 
 int main(){
-	cout << "hello world" << endl;
+	int n; //quantidade de vezes que o usuario quer que imprima
+    cin >> n; 
+
+    for (int i = 0; i<=n; i++){ //declaro i, igualo a zero, leio o bloco for até a i for menor ou igual a qtd de vezes que o usuario deseja, e incremento
+        cout << i << endl; //printo o bloco do loop limitado;
+    }
 }

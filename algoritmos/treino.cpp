dbc1c9f821048aed2128a4d9bbac67f76c6405cf
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-28 14:47:51 -0300

    FIXING BUG DO ELIAS
    FIXING BUG

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,37 +2,6 @@
 using namespace std;
 
 
-int main()
-{
-    int day;
-    cin >> day;
-
-    switch(day)
-    {
-        case 1:
-            cout <<"Domingo \n";
-            break;
-        case 2:
-            cout <<"Segunda \n";
-            break;
-        case 3:
-            cout <<"Terça \n";
-            break;
-        case 4:
-            cout <<"Quarta \n";
-            break;
-        case 5:
-            cout <<"Quinta \n";
-            break;
-        case 6:
-            cout <<"Sexta \n";
-            break;
-        case 7:
-            cout <<"Sábado \n";
-            break;
-        default:
-            cout <<"Dia inválido";
-    }
-
-    return 0;
+int main(){
+	cout << "hello world" << endl;
 }

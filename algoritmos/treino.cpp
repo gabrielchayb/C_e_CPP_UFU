5c0fead982a0671aea282ea2b436f29d15e62562
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-28 20:38:34 -0300

    teoria - ponteiro printado dinamico

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -5,8 +5,7 @@ int main(){
     int x = 5; //crio uma var e coloco um numero
     int *ptr = &x; //crio um ponteiro e atribuo o valor de memoria de x
 
-    *ptr = 768; //alterando a posição do ponteiro dentro de uma variável 
-
-    cout << x << endl; // printando a nova POSIÇÃO do ponteiro na variável 
+    cout << *ptr + 1 << endl; // printando o ponteiro com um novo valor de maneira dinâmica 
+    cout << *ptr + 2 << endl; // 
 
 }

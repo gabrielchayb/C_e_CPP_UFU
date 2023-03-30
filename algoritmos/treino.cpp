15f876544d48519b590906a96112ba471180ea48
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 19:50:22 -0300

    fibonacci humberto

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,13 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b)
-{
-	float F, K;
-    for(int C=-10; C<=100; C++){
-        F = 9 * C / 5.0 + 32;
-        K = C + 273,15;
-        cout << "F: " << F << "K: " << K << endl; 
+int fibo (int N){
+    if(N==0 || N==1) return N; 
+    return fibo(N-1) + fibo(N-2);
+}
+
+
+
+int main (){
+    int N, a=0; 
+    cin >> N; 
+    while(a<N-1){
+        cout << fibo(a) << "";
+        a++;
     }
 
+    cout << fibo(N-1) << endl;
 }

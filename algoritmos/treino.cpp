2639dad044c48b5d14e72985dec62a54d8d83a11
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 14:47:07 -0300

    set - begin ()

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -3,34 +3,26 @@ using namespace std;
 
 
 int main(){
-	// Declarando um Mapa <char, elemento>
-	map <char, int> mp;
-    // Inserindo elementos no mapa
-	mp.insert({'a', 1});
-	mp.insert({'b', 2});
-	mp.insert({'c', 3});
-    // Número de elementos no mapa, neste caso: 3
-    cout << mp.size() << endl;
-    // Removendo o par <c, 3> basta apenas dizer a chave
-	mp.erase('c');
-	cout << mp.size() << endl;
-    //apaga o mapa 
-    mp.clear();
-    // Checando se a chave c existe
-	if(mp.find('c') != mp.end())
-		cout << "Existe" << endl;
-	else
-		cout << "Não existe" << endl;
-    // Acessando um elemento diretamente usando o 
-    // Operador []
-    cout << mp['a'] << endl;
-    // Criando um elemento usando o operador []
-    mp['d'] = 4;
-    cout << mp['d'] << endl;
-    // Modificando usando o operador []
-    mp['a'] = 5;
-    cout << mp['a'] << endl;
+	// Declarando um set de inteiros
+	set <int> st;
+    //Inserindo elementos no set
+    st.insert(1);
+    st.insert(10);
+    st.insert(4);
+    st.insert(6);
+    st.insert(1);
+    // Removendo elementos do set, caso ele exista
+    st.erase(1);
+    st.erase(101010);
+    // Referência para o primeiro elemento do set, podemos usar o begin() para acessar o menor elemento do Set.
+    auto ptr = st.begin();
+	// O primeiro é o menor, logo será 1
+	cout << *ptr << endl;
+
+
+
 
 
 	return 0;
+
 }

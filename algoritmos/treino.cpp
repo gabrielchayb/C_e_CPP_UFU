0e7d027a8560fea997ec425db91cd30ac7de11fa
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 20:15:56 -0300

    mdc sequencial

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,20 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibo (int N){
-    if(N==0 || N==1) return N; 
-    return fibo(N-1) + fibo(N-2);
+
+
+int mdc(int a, int b){ //funcao mdc padrão 
+    if(a == 0) return b;
+    return mdc(b%a, a);
 }
 
-
-
-int main (){
-    int N, a=0; 
+int main(){
+    int N, resp, x; //n = numero de numeros que tera, resposta sera a pos 1 do vector, e x sera o valor add no vector
     cin >> N; 
-    while(a<N-1){
-        cout << fibo(a) << "";
-        a++;
+    vector<int>v;
+    while(N--){
+        cin >> x; 
+        v.push_back(x);
     }
 
-    cout << fibo(N-1) << endl;
+    resp = v[0]; // coloca o valor de resp como a pos 1 do vector 
+
+    for(int i=1;i<v.size();i++){ // i começa na 2posição, percorre ate o vector acabar, e adiciona 1
+        resp = mdc(resp, v[i]); // atualiza o valor da pos 1 do vector, com a func mdc em cima dos parametros da pos 1 e da nova posição (v[i]);
+    }
+
+    cout << resp << endl;
 }

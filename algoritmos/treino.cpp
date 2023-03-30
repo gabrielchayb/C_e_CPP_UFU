426710a5757ed4d44ca2633137103247f77cc16c
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 19:30:50 -0300

    exhumberto conversor temp c for

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -3,22 +3,11 @@ using namespace std;
 
 int gcd(int a, int b)
 {
-	if (b == 0) return a;
-	return gcd(b, a%b);
-}
+	float F, K;
+    for(int C=-10; C<=100; C++){
+        F = 9 * C / 5.0 + 32;
+        K = C + 273,15;
+        cout << "F: " << F << "K: " << K << endl; 
+    }
 
-int lcm(int a, int b)
-{
-	int answer = a*b;
-	answer /= gcd(a, b);
-
-	return answer;
-}
-
-int main()
-{
-	int a, b;
-	cin >> a >> b;
-
-	cout << "Mínimo múltiplo comum de " << a << " e " << b << ": " << lcm(a,b) << endl;
 }

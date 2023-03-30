f521e62ef0dc32c6f06a95eb93b6126216ab924a
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 12:42:39 -0300

    filas - teoria

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,21 +1,18 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-//passagem de referência
-
-int f(int x){ // funcao fatorial
-    if(x == 0){ // caso base
-        return 1;
-    }
-    else{ // recursao
-        return x * f(x-1);
-    }
-}
 
 int main(){
-    int N;
-
-    cin >> N;
-
-    cout << N << f(N) << endl;
+	// Declarando uma fila de inteiros
+	queue <int> q;	
+    // PUSH = Inserindo elementos no FINAL da fila
+	q.push(1);
+	q.push(2);
+    // O front retorna o primeiro elemento (o mais antigo) da fila, nesse caso 1
+	cout << q.front() << endl;
+    // Removendo o elemento mais antigo, ou seja, o primeiro inserido na fila
+	q.pop();
+    // Imprimindo o tamanho da fila, neste caso será 2
+	cout << q.size() << endl;
+	return 0;
 }

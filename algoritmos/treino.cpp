ad0790634deb0859364c8c7b1473dd178d1b27a5
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 14:09:31 -0300

    fila de prioridade - teoria

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,25 +1,20 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int x, cont = 1, vetor[10];
-	
-	for (int i=0; i<10; i++) {
-		cin >> vetor[i];
-	}
-	
-	cin >> x;
-	
-	for (int i=0; i<10; i++) {
-		if (vetor[i] == x) {
-			cout << "SIM";
-			cont = 0;
-			break;
-		}
-	}
-	
-	if (cont == 1) {
-		cout << "NAO";
-	}
+
+int main(){
+	// Declarando uma Fila de Prioridade
+	priority_queue <int, vector <int>, greater <int>> pq;
+    // Inserindo elementos na Fila 
+	pq.push(1);
+	pq.push(4);
+	pq.push(2);
+    // O elemento no topo
+	cout << pq.top() << endl;
+	// Removendo o elemento do topo, nesse caso, o maior
+	pq.pop();
+    // A estrutura possui 3 elementos
+	cout << pq.size() << endl;
+    
+	return 0;
 }

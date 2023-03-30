e08dad32c7c276cf68e368542963be67deb090a8
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-30 14:38:27 -0300

    operações com MAP

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -9,12 +9,27 @@ int main(){
 	mp.insert({'a', 1});
 	mp.insert({'b', 2});
 	mp.insert({'c', 3});
-    // Ponteiro para o final do map 
-	auto ptr = mp.end();
-    // Movendo para acessar o último elemento do map
-    ptr--;
-    // Para acessar a chave usamos o first, e o elemento usamos o second
-    cout << "Chave" << ptr->first << " Elemento: " << ptr->second << endl; 
+    // Número de elementos no mapa, neste caso: 3
+    cout << mp.size() << endl;
+    // Removendo o par <c, 3> basta apenas dizer a chave
+	mp.erase('c');
+	cout << mp.size() << endl;
+    //apaga o mapa 
+    mp.clear();
+    // Checando se a chave c existe
+	if(mp.find('c') != mp.end())
+		cout << "Existe" << endl;
+	else
+		cout << "Não existe" << endl;
+    // Acessando um elemento diretamente usando o 
+    // Operador []
+    cout << mp['a'] << endl;
+    // Criando um elemento usando o operador []
+    mp['d'] = 4;
+    cout << mp['d'] << endl;
+    // Modificando usando o operador []
+    mp['a'] = 5;
+    cout << mp['a'] << endl;
 
 
 	return 0;

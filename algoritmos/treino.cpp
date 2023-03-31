d1c31fd8a231a0801bcaa49b5feaa04e77403110
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-31 18:27:46 -0300

    teste de primalidade simples

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,32 +2,17 @@
 using namespace std;
 
 
+int main()
+{
+	int p;
+	cin >> p;
 
-int main(){
-    int v[10], cont=0, x, ind[10], j=0; 
+	bool isPrime = true;
 
-    for(int i = 0; i<10; i++){
-        cin >> v[i]; // preenchi meu vetor com valores do usuario
-    }
+	for (int i = 2; i < p; i++)
+		if (p%i == 0) 
+			isPrime = false;
 
-    cout << "digite o numero de verificação: " << endl; 
-    cin >> x; //qual numero iremos checar?
-
-    for(int i = 0; i < 10; i++){
-        if(v[i]==x){ //se o numero estiver no vetor
-            cont++; //add 1 na variavel contadora
-            ind[j] = i; // coloque esse valor no meu vetor auxiliar, na minha posição auxiliar (j)
-            j++; // adicione 1 na pos aux do vetor aux
-        }
-    }
-
-    if(cont > 0){
-        cout << cont << endl; //se a var contadora for maior que zero imprima-a 
-        for (int i = 0; i < cont; i++){
-            cout << ind[i] << endl;  // se a var contadora for maior que zero, printe meu vetor aux , até que a posição dese valor seja igual a minha contadora 
-    }
-    }
-    else{
-        cout << "mia x" << endl;
-    }
+	if (isPrime) cout << p << " é um número primo" << endl;
+	else cout << p << " não é um número primo" << endl;
 }

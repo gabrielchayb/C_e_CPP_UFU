64f60122b2d033e6ee68755391db36316986522c
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-03-31 14:14:34 -0300

    exercicio - busca no vetor e impressao de indices

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -4,12 +4,30 @@ using namespace std;
 
 
 int main(){
-    int v[3];
+    int v[10], cont=0, x, ind[10], j=0; 
 
-    for (int i = 0; i<3; i++){
-        cin >> v[i]; 
-
+    for(int i = 0; i<10; i++){
+        cin >> v[i]; // preenchi meu vetor com valores do usuario
     }
 
-    cout << v.sizeof() << endl;
+    cout << "digite o numero de verificação: " << endl; 
+    cin >> x; //qual numero iremos checar?
+
+    for(int i = 0; i < 10; i++){
+        if(v[i]==x){ //se o numero estiver no vetor
+            cont++; //add 1 na variavel contadora
+            ind[j] = i; // coloque esse valor no meu vetor auxiliar, na minha posição auxiliar (j)
+            j++; // adicione 1 na pos aux do vetor aux
+        }
+    }
+
+    if(cont > 0){
+        cout << cont << endl; //se a var contadora for maior que zero imprima-a 
+        for (int i = 0; i < cont; i++){
+            cout << ind[i] << endl;  // se a var contadora for maior que zero, printe meu vetor aux , até que a posição dese valor seja igual a minha contadora 
+    }
+    }
+    else{
+        cout << "mia x" << endl;
+    }
 }

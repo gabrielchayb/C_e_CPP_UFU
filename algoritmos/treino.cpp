257e66e0910867697e1dedc5525698059e5cc1f6
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-05 11:53:35 -0300

    substituição no vetor

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,16 +2,25 @@
 
 using namespace std;
 
+vector<int>v, pos;
+
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int a,b,c,menor=10000;
-    cin >> a >> b >> c;
-    for(int i=1;i<=3;i++){
-        if(i==1 && (b*2)+(c*4) < menor) menor = (b*2)+(c*4);
-        if(i==2 && (a*2)+(c*2) < menor) menor = (a*2)+(c*2);
-        if(i==3 && (a*4)+(b*2) < menor) menor = (a*4)+(b*2);
+    int menor=1000,x,i; //variavel menor
+    for(i=0;i<10;i++){
+        cin >> x; //preenchi meu vetor
+        v.push_back(x); //joguei os valores de x no vetor
+        if(x < menor) menor = x; //descobre o menor valor "x" adicionado no vetor
     }
-    cout << menor << endl;
+    for(i=0;i<10;i++){ 
+        if(v[i]==menor){ //achei meus valores menores no v, 
+            pos.push_back(i); v[i] = -1; // troquei eles por -1
+        } 
+    }
+    cout << "Menor: " << menor << endl; //printa o menor valor do vetor v
+    cout << "Ocorrencias: "; 
+    for(i=0;i<pos.size()-1;i++) cout << pos[i] << " "; //indices que o menor valor apareceu (guarde no meu vetor auxiliar pos)
+    cout << pos[pos.size()-1] << endl;
+    for(i=0;i<v.size()-1;i++) cout << v[i] << " ";
+    cout << v[v.size()-1] << endl; // printa o vetor resultante
 }
+    

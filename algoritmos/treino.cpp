84fa70f9db4b5ace18fe498fa44de26e81f43a86
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-06 19:40:39 -0300

    imprima os dois menores valores do vetor

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -3,15 +3,31 @@ using namespace std;
 
 
 int main(){
-    int N, menor = 1; 
+    int N, menor1, menor2; 
     cin >> N; 
     int v[N];
     for (int i = 0; i < N; i++){
         cin >> v[i]; 
-        if( v[i] < menor) { 
-            menor = v[i];
+        if( v[i] < menor1) { 
+            menor1 = v[i];
+        }
+
+    }
+
+    for(int i = 0; i<N; i++){
+        if (v[i] < menor2 && v[i] != menor1){
+            menor2 = v[i];
         }
     }
+
+    if (menor1 == menor2){
+        cout << menor1 << menor1 << endl; 
+    }
+
+    cout << menor1 <<endl;
+
+
+    
     
 
 

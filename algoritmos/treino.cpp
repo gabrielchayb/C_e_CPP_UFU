5502d6cd5af4e1563ede1ad814aec48e0bf1d30b
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-06 20:25:05 -0300

    soma de todos os valores de um vetor

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -4,27 +4,16 @@ using namespace std;
 
 
 int main(){
-    int N, m=0; 
-    cin >> N; 
-    int v[N+1];
-    
-    for (int i = 0; i < N; i++){
-        cin >> v[i]; 
-        m += v[i];
-    }
-
-    double dp=0;
-
-    
-
-    m = m/N;
-
-    for (int i = 0; i < N; ++i) {
-        dp+= pow(v[i] - m, 2);
-    }
-    dp= sqrt(dp / N);
-
-    cout << dp << endl;
+    int soma = 0;
+    int N; 
+    cin >> N;
+    int v[N];
+    for(int i = 0; i < N; i++){ 
+        cin >> v[i];
+        soma = soma + v[i];
+    } 
+     
+    cout << soma << endl; 
 
 
 }

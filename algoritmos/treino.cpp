2b8c47feb4e84a0a37968be08843b4ca3b6bf4b4
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-06 19:26:39 -0300

    maior valor do vetor

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-vector<ll>v;
 
 int main(){
-    ll N,x,total=1,i,maior=0;
-    cin >> N;
-    for(i=1;i<=N;i++){
-        cin >> x;
-        v.push_back(x);
+    int N, menor = 1; 
+    cin >> N; 
+    int v[N];
+    for (int i = 0; i < N; i++){
+        cin >> v[i]; 
+        if( v[i] < menor) { 
+            menor = v[i];
+        }
     }
-    for(i=1;i<v.size()-1;i++){ //percorre todos menos pos 0 e pos final
-        if(v[i-1]<0 && v[i]<0) total *= v[i-1]*v[i]*v[v.size()-1];
-        else total *= v[i-1]*v[i]*v[i+1];
-        if(total > maior) maior = total;
-        total=1;
-    }
-    cout << maior << endl;
+    
+
+
+
+    
+
+
+
 }
 
     

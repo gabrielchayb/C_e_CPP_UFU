ecc4b99d36408e5b797c5d3b4fc99d0aba607608
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-07 17:28:11 -0300

    multiplicação de b por e mod m

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long int fastMultiplication(long long int x, long long int y)
+long long int fastMultiplication(long long int x, long long int y, long long m)
 {
 	if (x == 0) // Caso base
 		return 0;
 
-	long long int answer = fastMultiplication(x/2, y); // Acha a resposta do nosso subproblema
-	answer = (2*answer); // Multiplica a nossa resposta por 2 e tira módulo m
+	long long int answer = fastMultiplication(x/2, y, m); // Acha a resposta do nosso subproblema
+	answer = (2*answer)%m; // Multiplica a nossa resposta por 2 e tira módulo m
 
 	if (x%2 == 0) // Checa se x é par
 		return answer;
 
-	return (answer + y);
+	return (answer + y)%m;
 }
 
 int main()
 {
-	long long int b, e;
-	cin >> b >> e;
+	long long int b, e, m;
+	cin >> b >> e >> m;
 
-	cout << fastMultiplication(b, e) << endl;
+	cout << fastMultiplication(b, e, m) << endl;
 }
-
     

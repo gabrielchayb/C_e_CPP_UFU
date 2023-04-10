ab4a86b54331742c4be09c8c89892fa9a8070909
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-10 19:50:03 -0300

    metodo top down - PD - fibonacci

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -3,18 +3,26 @@ using namespace std;
 
 const int MAXN = 20;
 
+int dp[MAXN]; // dp[i] = i-ésimo termo da sequência de Fibonacci
+// Inicialmente, todos os valores da dp são 0
+
+int solve(int i)
+{
+	if (dp[i] != 0) // Primeiro passo
+		return dp[i];
+
+	if (i == 1 || i == 2) // Segundo passo
+		return 1;
+
+	dp[i] = solve(i - 1) + solve(i - 2); // Terceiro passo
+	return dp[i];
+}
+
 int main()
 {
 	int n;
-	int dp[MAXN]; // dp[i] = i-ésimo termo da sequência de Fibonacci
 
 	cin >> n;
 
-	dp[1] = dp[2] = 1; // Casos base
-
-	for (int i = 3; i <= n; i++)
-		dp[i] = dp[i - 1] + dp[i - 2]; // Calculando dp[i]
-
-	cout << "N-ésimo termo de Fibonacci: " << dp[n] << endl;
+	cout << "N-ésimo termo de Fibonacci: " << solve(n) << endl;
 }
-    

9c274fe93dee45a3debd34ae2629abb1d80e06cb
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-10 19:09:08 -0300

    teoria - problemas de busca

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,39 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int v[NMAX];
-int aux[NMAX];
-
-void MERGE_SORT(int L, int R){
-
-	if(L == R) return;
-	int meio = (L + R)/2; //arredonda pra baixo
-	MERGE_SORT(L, meio);
-	MERGE_SORT(meio + 1, R);
-	int a;
-	int pos1 = L;
-	int pos2 = meio + 1;
-	for(a=L;a<=R;a++){
-	        //caso a pos1 ultrapasse o limite do primeiro
-	        //o aux vira o v de pos2, sendo o aux oq vai ordenar td
-	        if(pos1 == meio+1){
-	                aux[a] = v[pos2];
-	                pos2++;
-	        }
-		else if(pos2 == R+1){
-			aux[a] = v[pos1];
-			pos1++;}
-		//os dois if de baixo é a parte da ord em si
-		else if(v[pos1] <= v[pos2]){
-			aux[a] = v[pos1];
-			pos1++;}
-		else{
-			aux[a] = v[pos2];
-			pos2++;}
-	}
-	for(a=L;a<=R;a++){
-		v[a] = aux[a];
-	}
+//problemas de busca
+int main(){
+	/*busca (problema, estratégia):
+    adicionar o estado inicial do problema ao conjunto de estados
+    laço de repetição:
+        se não há estados candidatos para explorar retorna falha
+        escolher outro estado do conjunto de estados para expandir a busca de acordo com a estratégia
+        se o estado satisfaz a condição de objetivo retorna a solução
+        expandir a busca, acrescentando mais estados possíveis ao conjunto de estados */
 }
-
     

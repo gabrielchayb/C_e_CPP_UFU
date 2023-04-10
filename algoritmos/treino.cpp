6091bdfff6446bafc0a1aab38d58048f28520e6f
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-09 21:36:45 -0300

    funções que tiram acentuação de strings

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,33 +1,62 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <iconv.h>
+#include<bits/stdc++.h>
+#include<locale>
 using namespace std;
 
+string convertAccentChars(const string& str) {
+    iconv_t cd = iconv_open("ASCII//TRANSLIT", "UTF-8");
 
-long long int fastMultiplication(long long int x, long long int y)
-{
-	if (x == 0)
-		return 0;
+    if (cd == (iconv_t)-1) {
+        cerr << "Erro ao abrir o conversor\n";
+        return str;
+    }
 
-	long long int answer = fastMultiplication(x/2, y); 
-	answer = (2*answer); 
+    string out;
+    size_t inbytesleft = str.size();
+    char* inbuf = const_cast<char*>(str.c_str());
+    std::size_t outbytesleft = inbytesleft * 3;
+    char* outbuf = new char[outbytesleft];
+    char* outbufp = outbuf;
 
-	if (x%2 == 0) 
-		return answer;
+    if (iconv(cd, &inbuf, &inbytesleft, &outbufp, &outbytesleft) == (size_t)-1) {
+        cerr << "Erro ao converter a string\n";
+        delete[] outbuf;
+        iconv_close(cd);
+        return str;
+    }
 
-	return (answer + y);
+    out.assign(outbuf, outbufp - outbuf);
+
+    delete[] outbuf;
+    iconv_close(cd);
+
+    return out;
 }
 
-long long int fastExponentiation(long long int b, long long int e)
-{
-	if (e == 0)
-		return 1;
-
-	long long int answer = fastExponentiation(b, e/2); 
-	answer = fastMultiplication(answer, answer); 
-
-	if (e%2 == 0) 
-		return answer;
-
-	return fastMultiplication(answer, b); 
+int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    string str;
+    while(getline(cin,str)){
+    string out = convertAccentChars(str);
+    for (int aux=0; aux<out.size(); aux++){
+        if(out[aux]>='A'&& out[aux]<='Z' || out[aux]>='a'&& out[aux]<='z'){
+        if(out[aux]>='A'&& out[aux]<='Z')out[aux]=char(out[aux]+32);
+        }
+        else{
+            out.erase(aux,1);
+            aux--;
+        }
+    }
+    cout<<out<<endl;
+    string ajuda=out;
+    reverse(ajuda.begin(),ajuda.end());
+    if(ajuda==out)cout<<"sim\n";
+    else cout<<"nao\n";
+     
+}
+    return 0;
 }
 
     

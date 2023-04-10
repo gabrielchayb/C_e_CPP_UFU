44481582239a46c49ce6910f92bd0b26d704706b
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-10 19:14:37 -0300

    problema da moeda - estrategia gulosa

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,13 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-//problemas de busca
+//busca gulosa
+
 int main(){
-	/*busca (problema, estratégia):
-    adicionar o estado inicial do problema ao conjunto de estados
-    laço de repetição:
-        se não há estados candidatos para explorar retorna falha
-        escolher outro estado do conjunto de estados para expandir a busca de acordo com a estratégia
-        se o estado satisfaz a condição de objetivo retorna a solução
-        expandir a busca, acrescentando mais estados possíveis ao conjunto de estados */
+    int V, N;
+
+    // Leia o número de moedas e o valor que queremos representar, respectivamente
+    cin >> N >> V;
+
+    int S[N];
+
+    // Ler todas as moedas
+    for (int i = 0; i < N; i++)
+    {
+        cin >> S[i];
+    }
+
+    // Estratégia Gulosa
+    int moedas = 0;
+    for (int i = N - 1; i >= 0; i--) //percorro meu conjunto 
+    {
+        while (V >= S[i]) //enquanto meu valor for maior que as posições do meu conjunto
+        {
+            moedas += 1; //adicione mais um nas moedas
+            V -= S[i]; // e v = v - s[i]
+        }
+    }
+
+    cout << moedas << endl;
 }
+
     

ed1e3aa6091f21cea85efd79c2e12eba055bbb3e
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-14 13:16:01 -0300

    problema da mochila

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,36 +1,83 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-#define N 100000
-typedef long long int ll;
-ll a[N];
 
-int search(int L, int R, int value) {
-	int mid = (L + R) / 2; 	// Meio do intervalo
-	
-	if(L > R)				// Intervalo invalido
-		return -1;			// Valor não encontrado
-	
-	if(a[mid] == value)		// O valor foi encontrado
-		return mid;
-	
-	if(a[mid] > value) 		// Descarto o intervalo à esquerda
-		return search( mid + 1, R, value);
-	else					// Descarto o intervalo à direita
-		return search( L, mid - 1, value);
-}
+const int MAXN = 1010;
+const int MAXS = 1010;
+const int INF = 1000000010;
 
+int main()
+{
+	int n, S;
+	int w[MAXN], v[MAXN], dp[MAXN][MAXS];
 
-int main() {
-    ll n;
-    cin>>n;
-    for(ll i=0; i<n; i++){
-        cin>>a[i];
-    }
-    sort(a, a+n, greater<int>()); //ordena senao estiver ordenado
-    int x;
-    cin>>x; //valor desejado
-    int i=search(0, n-1, x);
-    if(i!=-1)cout<<i<<endl; //vai printar indexado de 0 a n-1, se quiser de 1 a n, só somar +1
-    else cout<<"N tem esse valor no vetor\n";
-     
+	bool opt[MAXN][MAXS]; // opt[i][j] = o i-ésimo item será escolhido no subconjunto ótimo de dp[i][j] ou não
+
+	cin >> n >> S;
+
+	for (int i = 1; i <= n; i++)
+		cin >> w[i] >> v[i];
+
+	dp[0][0] = 0;
+
+	for (int i = 1; i <= S; i++)
+		dp[0][i] = -INF;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j <= S; j++)
+		{
+			if (w[i] > j) 
+			{
+				opt[i][j] = false;
+				dp[i][j] = dp[i - 1][j];
+			}
+			else
+			{
+				if (dp[i - 1][j] <= dp[i - 1][j - w[i]] + v[i]) // A melhor opção é pegar o i-ésimo item
+				{
+					opt[i][j] = true;
+					dp[i][j] = dp[i - 1][j - w[i]] + v[i];
+				}
+				else // A melhor opção é não pegar o i-ésimo item
+				{
+					opt[i][j] = false;
+					dp[i][j] = dp[i - 1][j];
+				}
+			}
+		}
+	}
+
+	int s = 0; // ÍNdice que maximiza dp[n][s]
+	int answer = 0;
+
+	vector<int> items;
+
+	for (int i = 1; i <= S; i++)
+	{
+		if (answer < dp[n][i])
+		{
+			s = i;
+			answer = dp[n][i];
+		}
+	}
+
+	for (int i = n; i > 0; i--)
+	{
+		if (opt[i][s]) // Nós usamos o item i na resposta ótima
+		{
+			s -= w[i];
+			items.push_back(i);
+		}
+	}
+
+	reverse(items.begin(), items.end()); // Revertendo a resposta para ela ficar em ordem crescente
+
+	cout << "Soma máxima dos valores: " << answer << endl;
+
+	cout << "Itens do subconjunto ótimo: ";
+
+	for (int i = 0; i < (int)items.size(); i++)
+		cout << items[i] << " ";
 }

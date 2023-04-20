ffff04af3917d7346ac5b614e3342916aac347f9
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-20 19:11:37 -0300

    loop de inteiro de 1000 em 1000 ate 100k

---
+++ algoritmos/treino.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int i; 
+    for(i = 0; i < 100000;){
+        cout << i << endl;
+        i = i + 1000;
+        
+    }
+    
+}

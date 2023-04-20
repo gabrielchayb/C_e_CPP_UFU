14d183b93634ddb63310371bf7f28a3a4b31af7c
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-20 19:52:03 -0300

    exercicio 4 revisao
    EXERCÍCIO4Escreva um programa em linguagem C que leia um vetor de inteiros com 10 posições e, em seguida, mostre a posição onde se encontram o maior e o menor valor. As posições informadas para o usuário devem seguir a notação: 1 para a primeira até 10 para a décima posição.

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,21 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define INF 999999999
+
+int v[11], pos_maior[11], pos_menor[11];
+
 
 int main(){
-  int v[10], qtd=0, soma=0; 
-  for (int i = 0; i < 10; i++){
-    cin >> v[i];
-    if(v[i] < 0){
-      qtd++;
+    int i, maior=-INF, menor=INF, a=1, b=1;
+    
+    for(i = 0; i < 10; i++){
+        cin >> v[i];
+        if(v[i]>maior){
+            maior= v[i];
+        }
+        if(v[i] < menor){
+            menor = v[i];
+        }
+
     }
-    else if(v[i] >= 0){
-      soma = soma + v[i];
+    for(i=1; i<=10; i++){
+        if(v[i]== maior){
+            maior = pos_maior[i];
+        }
+        if(v[i] == menor){
+            menor = pos_menor[i];
+        }
     }
-  }
-   
-    cout << qtd << endl; 
-    cout << soma << endl;
 
-
-
+    cout << "as posicoes sao:"; 
+    for(i=1; i<a; i++){
+        cout << pos_maior << endl;
+    }
+    for(i=1; i<b; i++){
+        cout << pos_menor << endl; 
+    }
 }

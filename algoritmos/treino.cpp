72414e665290f12900601214ec3c7dd783a865de
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-04-20 20:06:01 -0300

    exercicio 5 revisao
    Escreva um programa em linguagem C que leia um vetor de 20 posições e indique se existem valores repetidos, escrevendo na tela os valores.

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,36 +2,16 @@
 using namespace std;
 #define INF 999999999
 
-int v[11], pos_maior[11], pos_menor[11];
-
 
 int main(){
-    int i, maior=-INF, menor=INF, a=1, b=1;
-    
-    for(i = 0; i < 10; i++){
-        cin >> v[i];
-        if(v[i]>maior){
-            maior= v[i];
+    int v[20]; 
+    for(int i = 1; i <20; i++){
+        cin >> v[i]; 
+        for(int j = 1; j < i; j++){
+            if(i!=j && v[j]==v[i]){
+                cout << v[i] << endl;
+                break;
+            }
         }
-        if(v[i] < menor){
-            menor = v[i];
-        }
-
-    }
-    for(i=1; i<=10; i++){
-        if(v[i]== maior){
-            maior = pos_maior[i];
-        }
-        if(v[i] == menor){
-            menor = pos_menor[i];
-        }
-    }
-
-    cout << "as posicoes sao:"; 
-    for(i=1; i<a; i++){
-        cout << pos_maior << endl;
-    }
-    for(i=1; i<b; i++){
-        cout << pos_menor << endl; 
-    }
+    }    
 }

2e0240c66775e8ef4242e3f273b14fd912ee4d07
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-15 17:38:29 -0300

    EX 1 HUMBERTO - SOMA DOS ELEMENTOS DA MATRIZ

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define INF 999999999
-
 
 int main(){
-    int v[20]; 
-    for(int i = 1; i <20; i++){
-        cin >> v[i]; 
-        for(int j = 1; j < i; j++){
-            if(i!=j && v[j]==v[i]){
-                cout << v[i] << endl;
-                break;
-            }
+    int m[3][3];
+    int soma = 0; 
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            cin >> m[i][j];
         }
-    }    
+    }
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            soma = soma + m[i][j]; 
+        }
+    }
+
+    cout << soma << endl;
+
 }

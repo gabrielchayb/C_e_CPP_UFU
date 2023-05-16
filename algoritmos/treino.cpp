205ba9b77118e1436218534e91ea4fd269787441
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-16 19:57:59 -0300

    EX 1 STRUCT HUMBERTO media de 10 notas de alunos diferentes com 3 notas cadas

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,20 +2,22 @@
 
 using namespace std;
 
+ struct aluno{
+        int num_aluno;
+        float n1, n2, n3;
+        float media;
+ };
+
 int main() {
-    // Array de soma com todas suas casas em '0'
-    int s[3] = {0, 0, 0}, num;
-
-    // For duplo para capturar os 9 valores, e acrescentando a soma de sua respectiva linha
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> num;
-            s[i] += num;
-        }
+    struct aluno a[10];
+    for(int i = 0; i < 10; i++){
+        cin >> a[i].num_aluno;
+        cin >> a[i].n1;
+        cin >> a[i].n2;
+        cin >> a[i].n3;
+        a[i].media = a[i].n1 + a[i].n2 + a[i].n3 / 3.0;
     }
 
-    // Para cada linha, imprima sua soma
-    for(int i = 0; i < 3; i++) {
-        cout << "Linha " << i << ": " << s[i] << endl;
-    }
+
+
 }

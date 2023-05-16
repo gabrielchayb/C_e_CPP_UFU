a56626dfa43b288898a1327e62f4da97645a2f0a
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-16 10:11:42 -0300

    ex 3 humberto
    1) Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais elementos. Escreva ao final a matriz obtida

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,47 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int m[5][5]; 
 
- main(){
+    for (int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            if (i == j){
+                m[i][j] = 1; // diagonal principal
+            }
+            else{
+                m[i][j] = 0; //resto da matriz
+            }
+        }
+    }
 
-  int A[2][3], B[2][3] ,C[2][3];
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            cout << m[i][j];
+        }
+    }
 
-  // Obtendo a primeira matriz
-  for(int i=0;i<2;++i){
-   for(int j=0;j<3;++j){
-     printf("A[%d][%d]: ",i,j);
-     scanf("%d", &A[i][j]);
-   }
-  }
- 
-  // Afasta a matriz A da matriz B
-  printf("\n");
-
-  // Obtendo a segunda matriz
-  for(int i=0;i<2;++i){
-   for(int j=0;j<3;++j){
-     printf("B[%d][%d]: ",i,j);
-     scanf("%d", &B[i][j]);
-   }
-  }
- 
-  printf("\n");
-
-  // Calculando e exibindo a soma
-  for(int i=0;i<2;++i){
-   for(int j=0;j<3;++j){
-     C[i][j] = A[i][j] + B[i][j];
-     /* \t é utilizado para organização
-     da matriz tendo a função de tabulação*/
-     printf("%d\t",C[i][j]);
-     /* Quando j assumir valor 2 ocorrerá
-     uma quebra de linha, caracterizando
-     a estrutura da matriz*/
-      if (j==2) printf("\n");
-   }
-  }
- 
-  printf("\n");
-  system ("pause");
-
- }
+}

6fb21adc5340f4e099a2d6f7f8d93ea984e61a15
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-16 17:38:38 -0300

    lendo uma matriz quadrada e printando a posicao do menor valor
    Crie um programa capaz de ler os dados de uma matriz quadrada de inteiros. Ao final da leitura o programa deverá imprimir o número da linha e da coluna que contém o menor dentre todos os números lidos.

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,44 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int A[3][2], B[3][2], igual;
 
-    for(int i = 0; i < 3; i++){
-        for (int j = 0; j < 2; j++){
+int main(){
+    int A[2][2]; 
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
             cin >> A[i][j];
         }
-    } 
+    }
 
-    for(int i = 0; i < 3; i++){
-        for (int j = 0; j < 2; j++){
-            cin >> B[i][j];
-        }
-    } 
+    int menor = A[1][1];
 
     for(int i = 0; i < 2; i++){
-        for (int j = 0; j < 3; j++){
-            if(A[i][j] == B[i][j]){
-                igual = 1;
-            }
-            else{
-                igual = 0;
+        for(int j = 0; j < 2; j++){
+            if(A[i][j] < menor){
+                menor = A[i][j];
+                cout << i << "" << j << endl;
             }
         }
-    } 
-
-    if(igual == 1){
-        cout << "As matrizes sao iguais" << endl;
     }
 
-    else{
-        cout << "as matrizes são diferentes nas seguintes posiçoes: " << endl;
-        for(int i = 0; i < 2; i++){
-            for (int j = 0; j < 3; j++){
-                if(A[i][j] != B[i][j]){
-                    cout<<i<<" "<<j<<endl;
-                }   
-            }
-        } 
-    }
+
 }

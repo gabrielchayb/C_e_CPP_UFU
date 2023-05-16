9111c767564b95cce14a712686626e29b652e877
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-16 19:45:31 -0300

    soma das linhas de uma matriz

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,23 +1,21 @@
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
+int main() {
+    // Array de soma com todas suas casas em '0'
+    int s[3] = {0, 0, 0}, num;
 
-int main(){
-    int m[8][8], soma;
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            cin >> m[i][j];
+    // For duplo para capturar os 9 valores, e acrescentando a soma de sua respectiva linha
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> num;
+            s[i] += num;
         }
     }
 
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            if(i == j){
-                soma = soma + m[i][j];
-            }
-        }
+    // Para cada linha, imprima sua soma
+    for(int i = 0; i < 3; i++) {
+        cout << "Linha " << i << ": " << s[i] << endl;
     }
-    
-    cout << soma << endl;
-
 }

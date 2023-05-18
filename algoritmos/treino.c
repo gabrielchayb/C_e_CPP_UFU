c5ac96275169b9846044df7c47203e9e0ec5009e
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-18 20:26:09 -0300

    exercicio 5
    Crie uma estrutura denominada Retângulo. Essa estrutura deverá conter o ponto superior esquerdo e o ponto inferior direito do retângulo. Cada ponto é definido pela estrutura Ponto, que contém as posições X e Y. Faça um programa que declare e leia uma estrutura Retângulo e exiba a área, o perímetro e o comprimento da diagonal  desse retângulo.

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -2,15 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct pontos{
-    int x, y, z;
+struct ret{
+    int x, y;
 };
 
-typedef struct pontos pontos;
+typedef struct ret ret;
 
 int main (){
-    pontos v;
-    scanf("%d %d %d", &v.x, &v.y, &v.z);
-    printf("Distancia: %lf\n", sqrt(v.x*v.x + v.y*v.y + v.z*v.z));
+    pontos inf_dir, sup_dir, inf_esq, sup_esq;
+    scanf("%d %d", &sup_esq.x, &sup_esq.y);
+    scanf("%d %d", &inf_dir.x, &inf_dir.y);
+    int DX = sup_esq.x - inf_dir.x;
+    int DY = sup_esq.y - inf_dir.y;
+    double diagonal = sqrt(DX*DX+DY*DY);
+    printf("Comprimento diagonal: %lf", diagonal);
+    int b,h;
+    for(i=1, i<=diagonal, i++){
+        for(j+1, j <=diagonal, j++){
+            if(i*i + j*j = diagonal*diagonal){
+                b = i;
+                h = j;
+                verifica = 1;
+                break;
+            }
+        }
+        if(verifica=1) break;
+    }
+
+    printf("Area %d:", b*h);
+    printf("Perimetro %d:", b*2+h*2);
+    printf("Diagonal %d:", diagonal;)
+    
+
+    
+    
+
 
 }

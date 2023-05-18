d1e4814751b0b2366392d650c172b816bd37449c
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-18 19:46:29 -0300

    questão 2 humberto
    Crie uma estrutura que represente um atleta. A estrutura deve conter o nome do atleta, seu esporte, idade, peso e altura. Escreva um programa que leia os dados de cinco atletas em um vetor. Calcule e exiba os nomes do atleta mais alto e do atleta mais velho.

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -1,24 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Questao 1
-struct pessoa{
-    char nome[50], endereco[100];
+struct atleta{
+    char nome[50], esporte[100];
     int idade;
+    double peso, altura;
 };
 
-typedef struct pessoa pessoa;
+typedef struct atleta atleta;
 
 int main (){
-    pessoa v[5];
+    double alto=-100;
+    int velho=-100;
+    atleta v[5];
+    char maisalto[50], maisvelho[50];
     for(int i=0; i<5; i++){
         setbuf(stdin,NULL);
         gets(v[i].nome);
         setbuf(stdin,NULL);
-        gets(v[i].endereco);
-        scanf("%d", &v[i].idade);
+        gets(v[i].esporte);
+        scanf("%d %lf %lf", &v[i].idade, &v[i].peso, &v[i].altura);
+        if(v[i].altura>alto){
+            for(int j=0; j<50; j++){
+                maisalto[j]='\0';
+            }
+            alto=v[i].altura;
+            strcpy(maisalto,v[i].nome);
+        }
+        if(v[i].idade>velho){
+            for(int j=0; j<50; j++){
+                maisvelho[j]='\0';
+            }
+            velho=v[i].idade;
+            strcpy(maisvelho,v[i].nome);
+        }
     }
     for(int i=0; i<5; i++){
-        printf("a pessoa %s mora no endereco %s e tem %d de idade\n", v[i].nome, v[i].endereco, v[i].idade);
+        printf("o atleta %s pratica o esporte %s e tem %d de idade, pesa %.3lf kg e tem %.2lf m de altura\n", v[i].nome, v[i].esporte, v[i].idade, v[i].peso, v[i].altura);
     }
+    printf("o atleta mais velho eh %s\n", maisvelho);
+    printf("o atleta mais alto eh %s\n", maisalto);
+    
 }

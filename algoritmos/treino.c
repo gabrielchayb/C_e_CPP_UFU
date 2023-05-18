2154d1d507cf273f2ed1a3bae8215fb901c1afce
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-18 20:07:38 -0300

    problema 4
    Crie uma estrutura que represente um ponto em 3 dimensões (x,y,z). Em seguida, declara e leia do teclado um ponto e exiba a distância dele até a origem das coordenadas, isto é, o ponto (0,0,0).

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -1,37 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct atleta{
-    char nome[50], esporte[100];
-    int idade;
-    double peso, altura;
+struct pontos{
+    int x, y, z;
 };
 
-typedef struct atleta atleta;
+typedef struct pontos pontos;
 
 int main (){
-    atleta v[5];
-    atleta aux;
+    pontos v;
+    scanf("%d %d %d", &v.x, &v.y, &v.z);
+    printf("Distancia: %lf\n", sqrt(v.x*v.x + v.y*v.y + v.z*v.z));
 
-    for(int i=0; i<5; i++){
-        setbuf(stdin,NULL);
-        gets(v[i].nome);
-        setbuf(stdin,NULL);
-        gets(v[i].esporte);
-        scanf("%d %lf %lf", &v[i].idade, &v[i].peso, &v[i].altura);
-    }
-
-    for(int i = 0; i < 4; i++){
-        for(int j = i+1; j < 5; j++){
-            if(v[i].altura > v[j].altura){
-                aux = v[i];
-                v[i] = v[j];
-                v[j] = aux;
-            }
-        }
-    }
-
-    for(int i = 0; i < 5; i++){
-        printf("%d", &v[i].altura)
-    }
 }

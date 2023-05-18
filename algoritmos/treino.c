a9c55ce0dc4ba94f1de0ff5aa44bd1f1af584196
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-18 20:00:44 -0300

    questão 3
    Crie uma estrutura que represente um atleta. A estrutura deve conter o nome do atleta, seu esporte, idade, peso e altura. Escreva um programa que leia os dados de cinco atletas em um vetor. Em seguida, ordene os atletas pela altura. Exiba o vetor ordenado.

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -10,35 +10,28 @@ struct atleta{
 typedef struct atleta atleta;
 
 int main (){
-    double alto=-100;
-    int velho=-100;
     atleta v[5];
-    char maisalto[50], maisvelho[50];
+    atleta aux;
+
     for(int i=0; i<5; i++){
         setbuf(stdin,NULL);
         gets(v[i].nome);
         setbuf(stdin,NULL);
         gets(v[i].esporte);
         scanf("%d %lf %lf", &v[i].idade, &v[i].peso, &v[i].altura);
-        if(v[i].altura>alto){
-            for(int j=0; j<50; j++){
-                maisalto[j]='\0';
+    }
+
+    for(int i = 0; i < 4; i++){
+        for(int j = i+1; j < 5; j++){
+            if(v[i].altura > v[j].altura){
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
             }
-            alto=v[i].altura;
-            strcpy(maisalto,v[i].nome);
-        }
-        if(v[i].idade>velho){
-            for(int j=0; j<50; j++){
-                maisvelho[j]='\0';
-            }
-            velho=v[i].idade;
-            strcpy(maisvelho,v[i].nome);
         }
     }
-    for(int i=0; i<5; i++){
-        printf("o atleta %s pratica o esporte %s e tem %d de idade, pesa %.3lf kg e tem %.2lf m de altura\n", v[i].nome, v[i].esporte, v[i].idade, v[i].peso, v[i].altura);
+
+    for(int i = 0; i < 5; i++){
+        printf("%d", &v[i].altura)
     }
-    printf("o atleta mais velho eh %s\n", maisvelho);
-    printf("o atleta mais alto eh %s\n", maisalto);
-    
 }

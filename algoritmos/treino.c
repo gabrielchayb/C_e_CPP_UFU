e5545d4beec0fa031c2be935a59e9f8f33a82017
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-19 18:39:48 -0300

    exercicio 4 strings - palindromo
    Faça um programa que leia uma string e imprima uma mensagem dizendo se ela é um palíndromo ou não. Um palíndromo é uma palavra ou frase que tem a propriedade de poder ser lida tanto da direita para a esquerda como da esquerda para a direita. Exemplos:ovoararareverossosocorrammesubinoonibusemmarrocos

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main(){
-    char str1[20];
-    gets(str1);
-    for(int i = strlen(str1)-1; i>=0; i--){
-        printf("%c", str1[i]);
+int main() {
+    char str[100];
+    int i, j;
+    int ehPalindromo = 1;
+
+    printf("Digite uma palavra ou frase: ");
+    gets(str);
+
+    // Verificando se é um palíndromo
+    for (i = 0, j = strlen(str) - 1; i < j; i++, j--) {
+        if (str[i] != str[j]) {
+            ehPalindromo = 0;
+            break;
+        }
     }
-    printf("\n");
+
+    if (ehPalindromo) {
+        printf("%s é um palíndromo.\n", str);
+    } else {
+        printf("%s não é um palíndromo.\n", str);
+    }
+
+    return 0;
 }

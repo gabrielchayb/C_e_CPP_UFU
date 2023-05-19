95f0d002c0cb91c7b6a075692f87845d22a6f595
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-19 18:14:33 -0300

    exercicio 1 strings lab
    Escreva um programa que leia uma string e converta todos os seus caracteres em caracteres maiúsculos. Imprima a string convertida. Dica: subtrair 32 dos caracteres cujo código ASCII estiverem entre 97 e 122.

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){    	
-    char str1[50]; 
-	int n;
-	gets(str1);
-	n = strlen(str1);
-    printf("%d", n);
-
-    return 0;
+int main(){
+    char str1[200];
+    gets(str1);
+    int i;
+    int tam = strlen(str1);
+    for(i=0;i<tam;i++){
+        if(str1[i]>='a'&&str1[i]<='z'){
+            str1[i] -= 32;
+        }
+    }
+    printf("%s\n",str1);
 }

d0df04e579dc182bf834b3d3dd2d0deaa3cdbb10
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-19 14:13:37 -0300

    strings - TAMANHO da string

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -1,40 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-struct ponto{
-    int x,y;
-};
-typedef struct ponto ponto;
+int main(){    	
+    char str1[50]; 
+	int n;
+	gets(str1);
+	n = strlen(str1);
+    printf("%d", n);
 
-struct poligono{
-    ponto v[100];
-    int qtd; //contar quantos pontos o poligono utiliza
-};
-
-typedef struct poligono poligono;
-
-
-
-int main(){
-    poligono triangulo,retangulo;
-    printf("Leitura Coordenadas Triangulo: ");
-    int i;
-    for(i=0;i<3;i++){ //leitura coordenadas: //{5,3} {1,1} {1,5} 
-        scanf("%d %d",&triangulo.v[i].x,&triangulo.v[i].y);
-    }
-    printf("\nLeitura Coordenadas Retangulo: ");
-    for(i=0;i<4;i++){ //leitura coordenadas: //{1,1} {1,3} {5,1} {5,3}
-        scanf("%d %d",&retangulo.v[i].x,&retangulo.v[i].y);
-    }
-    printf("Coordenadas Triangulo: ");
-    for(i=0;i<3;i++){
-        printf("{%d,%d} ",triangulo.v[i].x,triangulo.v[i].y);
-    }
-    printf("\nCoordenadas Retangulo: ");
-    for(i=0;i<4;i++){
-        printf("{%d,%d} ",retangulo.v[i].x,retangulo.v[i].y);
-    }
-    
-    printf("\n");
+    return 0;
 }

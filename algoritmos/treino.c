6912bc2825623ca6a3be67465f88acdf9c12a241
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-19 18:24:06 -0300

    exercicio 3 strings - humberto
    Faça um programa que leia uma string e a inverta. A string invertida deve ser armazenada na mesma variável (ou seja, no mesmo vetor de caracteres). Em seguida, imprima a string invertida.

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -3,14 +3,10 @@
 #include <string.h>
 
 int main(){
-    char str1[200];
+    char str1[20];
     gets(str1);
-    int i;
-    int tam = strlen(str1);
-    for(i=0;i<tam;i++){
-        if(str1[i]>='a'&&str1[i]<='z'){
-            str1[i] -= 32;
-        }
+    for(int i = strlen(str1)-1; i>=0; i--){
+        printf("%c", str1[i]);
     }
-    printf("%s\n",str1);
+    printf("\n");
 }

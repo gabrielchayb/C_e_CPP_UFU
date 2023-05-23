b92724f7af047b4049e03c953ef5a202173acb10
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-22 21:09:21 -0300

    2) Construa um programa que leia dois vetores de caracteres. Calcule e apresente a quantidade de vezes que segunda string está contida dentro da primeira.

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Nome: Leandro Silva Pina de Campos
+//Matrícula: 12221bsi202
 int main(){
-    char str[200],str2[200];
-    fgets(str,200,stdin);
-    int tam = strlen(str);
-    str[tam-1] = '\0';
-    int i,j,a,qtd=0;
-    scanf("%d %d", &i, &j);
-    if(i>=j){
-        for(a=i;a>=j;a--) str2[qtd++] = str[a];
+    char str1[200],str2[200],str3[200];
+    gets(str1); //casas i=2 j=1
+    setbuf(stdin,NULL);
+    gets(str2); //as
+    setbuf(stdin,NULL);
+    int tam1 = strlen(str1);
+    int tam2 = strlen(str2);
+    int i,qtd=0,j,k=0,w;
+    for(i=tam2-1;i<tam1;i++){
+        w=0;
+        for(j=k;j<=i;j++){
+            str3[w++] = str1[j];
+        }
+        k++;
+        if(strcmp(str3,str2)==0) qtd++;
     }
-    else{
-        for(a=i;a<=j;a++) str2[qtd++] = str[a];
-    }
-    printf("%s\n",str2);
+    printf("%d\n",qtd);
 }

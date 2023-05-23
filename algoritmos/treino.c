a4ce0dc5dfb2b6d4f86e74e0b462ad81168e9d03
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-22 21:11:31 -0300

    4) Uma cantiga de roda popular é chamada "O sapo não lava o pé". A brincadeira consiste em cantar os versos e em seguida cantar repetidamente trocando todas as vogais por cada uma das 5 vogais, uma de cada vez. Considere o seguinte trecho de código em linguagem C. Altere o programa para atender aos seguintes requisitos. O programa deve fazer a leitura de uma vogal minúscula, deve substituir todas as vogais do verso pela vogal lida e apresentar novamente o verso, repetidamente até que um espaço seja lido. Caso a leitura resulte em caracter diferente de uma vogal ou de um espaço, deve-se avisar o usuário e fazer novamente a leitura.  #include <stdio.h> #include <stdlib.h> int main() {     char verso[9][30] = { "o sapo nao lava o pe",                           "nao lava porque nao quer",                           "ele mora la na lagoa",                           "nao lava o pe porque nao quer",                           "o sapo nao lava o pe",                           "nao lava porque nao quer",                           "ele mora la na lagoa",                           "nao lava o pe porque nao quer",                           "mas que chule" };      for (int i = 0; i < 9; i++)         printf("%s\n",verso[i]);     printf("\n");      return 0; }

--- algoritmos/treino.c
+++ algoritmos/treino.c
@@ -1,25 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-//Nome: Leandro Silva Pina de Campos
-//Matrícula: 12221bsi202
-int main(){
-    char str1[200],str2[200],str3[200];
-    gets(str1); //casas i=2 j=1
-    setbuf(stdin,NULL);
-    gets(str2); //as
-    setbuf(stdin,NULL);
-    int tam1 = strlen(str1);
-    int tam2 = strlen(str2);
-    int i,qtd=0,j,k=0,w;
-    for(i=tam2-1;i<tam1;i++){
-        w=0;
-        for(j=k;j<=i;j++){
-            str3[w++] = str1[j];
+//Questao 4
+int main()
+{
+    char c;
+    while(true)
+    {
+        setbuf(stdin,NULL);
+        scanf("%c", &c);
+        char verso[9][30] = { "o sapo nao lava o pe",
+
+                              "nao lava porque nao quer",
+
+                              "ele mora la na lagoa",
+
+                              "nao lava o pe porque nao quer",
+
+                              "o sapo nao lava o pe",
+ "nao lava porque nao quer",
+
+                              "ele mora la na lagoa",
+
+                              "nao lava o pe porque nao quer",
+
+                              "mas que chule"
+                            };
+
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c==' ')
+        {
+            if(c==' ')return 0;
+            for (int i = 0; i < 9; i++)
+            {
+                int t=strlen(verso[i]);
+                for(int j=0; j<t; j++)
+                {
+                    if(verso[i][j]=='a' || verso[i][j]=='e' || verso[i][j]=='i' || verso[i][j]=='o' || verso[i][j]=='u')
+                        verso[i][j]=c;
+                }
+            }
+            for (int i = 0; i < 9; i++)printf("%s\n",verso[i]);
+            printf("\n");
         }
-        k++;
-        if(strcmp(str3,str2)==0) qtd++;
+        else
+        {
+            printf("Digite outro caracter: \n");
+        }
     }
-    printf("%d\n",qtd);
+    return 0;
+
 }

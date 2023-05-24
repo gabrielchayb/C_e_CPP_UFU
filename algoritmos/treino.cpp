9ba5ebf9f12dd9ac36198bfc0c55b65393838229
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-24 18:01:01 -0300

    Faça um programa para verificar se uma matriz 3x3 forma um quadrado mágico.  Quadrado Mágico é uma tabela quadrada igual a intersecção de números em que a soma de cada coluna, de cada linha e das duas diagonais são iguais

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
+int somal[3], somac[3], somad1=0, somad2=0;
 int main(){
-     int maior=-999999;
-     int m[3][3];
-     for(int i=0; i<3;  i++){
-         for(int j=0; j<3; j++){
-             cin>>m[i][j];
-             if(m[i][j]>maior)maior=m[i][j];
-         }
-     }
-     for(int i=0; i<3;  i++){
-         for(int j=0; j<3; j++){
-             if(m[i][j]==maior)m[i][j]=-1;
-         }
-     }
-     int i, j;
-     for(i=0; i<3;  i++){
-         for(j=0; j<2; j++){
-             cout<<m[i][j]<<" ";
-         }cout<<m[i][j]<<endl;
-     }
-}
+    int m[3][3];
+    int i, j;
+    bool verifica=true;
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            cin>>m[i][j];
+            somal[i]=somal[i]+m[i][j];
+            somac[j]=somac[j]+m[i][j];
+            if(i==j)somad1=somad1+m[i][j];
+            if(i+j==2)somad2=somad2+m[i][j];
+        }
+    }
+    for(i=0; i<2; i++){
+        if(somal[i]!=somal[i+1])verifica=false;
+        if(somal[i]!=somad1)verifica=false;
+        if(somal[i]!=somad2)verifica=false;
+        if(somal[i]!=somac[i])verifica=false;
+        if(somac[i]!=somac[i+1])verifica=false;
+    }
+    if(verifica)cout<<"SIM"<<endl;
+    else cout<<"NAO"<<endl;
+    

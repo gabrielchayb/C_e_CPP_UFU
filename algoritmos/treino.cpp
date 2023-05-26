df85245926437abf075be86d81f7ab96fd1b1ff1
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-25 23:13:32 -0300

    exercicio 10 struct humberto - calcule a diferença entre duas datas

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,23 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum ano{ nada, janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro};
-typedef enum ano ano;
-//exercicio 9
-int main(){
+//exercicio 10
+struct data{
+    int dia;
     int mes;
-    char meses[13][20] = {"nada","Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
-    scanf("%d",&mes);
-    if(mes == janeiro) printf("%s\n, 31 dias",meses[mes]);
-    else if(mes == fevereiro) printf("%s\n, 28 dias",meses[mes]);
-    else if(mes == marco) printf("%s\n, 31 dias",meses[mes]);
-    else if(mes == abril) printf("%s\n, 30 dias",meses[mes]);
-    else if(mes == maio) printf("%s\n, 31 dias",meses[mes]);
-    else if(mes == junho) printf("%s\n, 39 dias",meses[mes]);
-    else if(mes == julho) printf("%s\n, 31 dias",meses[mes]);
-    else if(mes == agosto) printf("%s\n, 31 dias",meses[mes]);
-    else if(mes == setembro) printf("%s\n, 30 dias",meses[mes]);
-    else if(mes == outubro) printf("%s\n, 31 dias",meses[mes]);
-    else if(mes == novembro) printf("%s\n, 30 dias",meses[mes]);
-    else if(mes == dezembro) printf("%s\n, 31 dias",meses[mes]);
+    int ano;
+};
+
+typedef struct data data;
+
+int main(){
+    int verifica;
+    data d1,d2,aux;
+    int meses[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    scanf("%d/%d/%d",&d1.dia,&d1.mes,&d1.ano); setbuf(stdin,NULL);
+    scanf("%d/%d/%d",&d2.dia,&d2.mes,&d2.ano);
+    //definir d1 como o menor possivel, caso n seja
+    if(d1.dia + d1.mes*100 + d1.ano*10000 > d2.dia + d2.mes*100 + d2.ano*10000){
+        aux = d1;
+        d1 = d2;
+        d2 = d1;
+    }
+    //enquanto d1 n chegar na data d2 (ou seja, d1!=d2), incrementa 1 dia e vai contando
+    int total_dias=0;
+    while(d1.dia + d1.mes*100 + d1.ano*10000!=d2.dia + d2.mes*100 + d2.ano*10000){
+        total_dias++;
+        if( (d1.ano%4==0 && d1.ano%100!=0) || (d1.ano%400==0) ){
+            if(verifica==1){ //so pode passar uma vez para cada ano bissexto
+                verifica=0; 
+                total_dias++;
+            }
+        }
+        if(d1.mes == 13){ //qnd ultrapassar limite meses 
+            d1.mes = 1;
+            d1.ano++;
+            verifica=1;
+        }
+        if(d1.dia == meses[d1.mes]+1){ //qnd ultrapassar limite dias
+            d1.dia = 1;
+            d1.mes++;
+        }
+        d1.dia++;
+    }
+    printf("%d\n",total_dias);
 }

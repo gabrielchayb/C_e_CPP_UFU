909a19f53972a417c9d210d88ddfa96066a6fb27
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-25 22:44:55 -0300

    exercicio 9 -> enum humberto

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef union teste{
-    int x;
-    float y;
-} teste;
-
+enum ano{ nada, janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro};
+typedef enum ano ano;
+//exercicio 9
 int main(){
-    teste t1;
-    t1.y = 50.0;
-    printf(" %d ", t1.x);
-    printf(" %d ", t1.y);
-
-    t1.x = 50.0;
-    printf(" %d ", t1.x);
-    printf(" %d ", t1.y);
-
-    return 0;
+    int mes;
+    char meses[13][20] = {"nada","Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+    scanf("%d",&mes);
+    if(mes == janeiro) printf("%s\n, 31 dias",meses[mes]);
+    else if(mes == fevereiro) printf("%s\n, 28 dias",meses[mes]);
+    else if(mes == marco) printf("%s\n, 31 dias",meses[mes]);
+    else if(mes == abril) printf("%s\n, 30 dias",meses[mes]);
+    else if(mes == maio) printf("%s\n, 31 dias",meses[mes]);
+    else if(mes == junho) printf("%s\n, 39 dias",meses[mes]);
+    else if(mes == julho) printf("%s\n, 31 dias",meses[mes]);
+    else if(mes == agosto) printf("%s\n, 31 dias",meses[mes]);
+    else if(mes == setembro) printf("%s\n, 30 dias",meses[mes]);
+    else if(mes == outubro) printf("%s\n, 31 dias",meses[mes]);
+    else if(mes == novembro) printf("%s\n, 30 dias",meses[mes]);
+    else if(mes == dezembro) printf("%s\n, 31 dias",meses[mes]);
 }

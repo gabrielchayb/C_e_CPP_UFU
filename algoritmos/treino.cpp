b456e252fa0b8d5e2da6ec905559f572eb9edf3e
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-05-30 20:01:28 -0300

    exercício 1 - humberto numero NEPERIANO

--- algoritmos/treino.cpp
+++ algoritmos/treino.cpp
@@ -1,30 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int A[2][2];
-    int sim = 0;
+int main() {
+    double e = 1.0;
+    double termo = 1.0;
+    int n = 1;
 
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j++){
-            cin >> A[i][j];
-        }
+    while (termo > 1e-8) {
+        termo /= n;
+        e += termo;
+        n++;
     }
 
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j++){
-            if(A[i][j] != A[j][i]){
-                sim = 1;
-            }
-        }
-    }
+    cout << "O número neperiano (e) é: " << e << std::endl;
 
-    if(sim == 1){
-        cout << " a matriz n eh simetrica" << endl;
-    }
-    else{
-        cout << " a matriz  eh simetrica" << endl;
-    }
-
-
+    return 0;
 }

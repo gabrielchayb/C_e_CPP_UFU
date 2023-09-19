e7b12449fa0c2b2fa3e1e292a387e3705bce7be0
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-09-19 09:36:49 -0300

    EXERCICIO 2 - alocação dinamica em matrizes

--- algoritmos/algoritmos/treino.c
+++ algoritmos/algoritmos/treino.c
@@ -1,20 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
     int N; 
-    printf("digite o tamanho do vetor:");
+    printf("dimensao LIN e COL das matrizes X e Y: ");
     scanf("%d", &N);
-    int *v;
-    v = (int *) malloc(N*sizeof(int));
-    for(int i = 0; i < N; i++){
-        scanf("%d", &v[i]);
-    }
-    for(int i = 0; i < N; i++){
-        printf("%d", v[i]);
+    double **m1;
+    double **m2;
+    double **r;
+
+    m1 = (double **)malloc(N * sizeof(double *));
+    for (int i = 0; i < N; i++) {
+        m1[i] = (double *)malloc(N * sizeof(double));
     }
 
-    free(v);
-    
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            scanf("%lf", &m1[i][j]); // Usar %lf para double
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            printf("%lf\n", m1[i][j]); // Usar %lf para double
+        }
+    }
+
+    m2 = (double **)malloc(N * sizeof(double *));
+    for (int i = 0; i < N; i++) {
+        m2[i] = (double *)malloc(N * sizeof(double));
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            scanf("%lf", &m2[i][j]); // Usar %lf para double
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            printf("%lf\n", m2[i][j]); // Usar %lf para double
+        }
+    }
+
+    r = (double **)malloc(N * sizeof(double *));
+    for (int i = 0; i < N; i++) {
+        r[i] = (double *)malloc(N * sizeof(double));
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            r[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            printf("%lf\n", r[i][j]);
+        }
+    }
+
+    // Libere a memória alocada
+    for (int i = 0; i < N; i++) {
+        free(m1[i]);
+    }
+    free(m1);
+
+    for (int i = 0; i < N; i++) {
+        free(m2[i]);
+    }
+    free(m2);
+
+    for (int i = 0; i < N; i++) {
+        free(r[i]);
+    }
+    free(r);
+
+    return 0;
 }
